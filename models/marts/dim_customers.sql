2b27d8956d299583ad5fa26a81303fb2015bbc04
katherine lamoureux <katherine.lamoureux+1785855660@dbtlabsworkshop.com>
2026-08-05 16:50:09 +0000

    changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -15,7 +15,7 @@ orders as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status as order_Status
 
     from {{ ref('stg_jaffle_shop__orders') }}
 
